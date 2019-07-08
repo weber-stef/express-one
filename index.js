e249dbb1c73e7b152e2b85e66094e89405b30250
Stefan Wurck <vitatech@gmx.de>
2019-07-08 14:30:00 +0200

    app renamed to index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
-const port = 3000
+const port = 3000;
+console.log(app);
 
 app.get('/', (req, res) => res.send('hey there, big World'))
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
